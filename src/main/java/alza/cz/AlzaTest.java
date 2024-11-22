b0129c0ed7d313a058d03c93956b11731530f9a3
Zuzana Ondrackova <ondrac.zuzana@gmail.com>
2024-11-22 10:08:16 +0100

    Test completed.

--- src/main/java/alza/cz/AlzaTest.java
+++ src/main/java/alza/cz/AlzaTest.java
@@ -52,25 +52,79 @@ public class AlzaTest {
                         (By.xpath("//a[@href='/ecg-24-h05t2s2-d9911329.htm']"))).
                 click();
 
+
         //variables for expected name of item
-        var expectedName = browser.findElement
+        var expectedName = "Televize" + " " + browser.findElement
                 (By.cssSelector(".h1-placeholder"))
                 .getText();
 
         //Add item to the cart
         browserWait.until
                 (ExpectedConditions.elementToBeClickable
-                        (By.xpath("//a[@href='javascript:detailOrder();")))
+                        (By.cssSelector(".price-detail__buy-actions")))
                 .click();
 
-        //variables for actual name of item
-        var actualName = browser.findElement
-                (By.id("order-item-679752367")).
-                getText();
+        //close panel for helping
+        browserWait.until(ExpectedConditions.elementToBeClickable
+                        (By.id("chat-open-button")))
+                .click();
+
+        //go to the cart
+        browserWait.until
+                (ExpectedConditions.elementToBeClickable
+                        (By.id("varBToBasketButton")))
+                .click();
+
+
+        //variable for actual name of item in the basket
+        var actualName = browserWait.until
+                (ExpectedConditions.elementToBeClickable
+                (By.cssSelector(".mainItem")))
+                .getText();
+
+        //cart price for one item
+        var pomCartPrice1 = browserWait.until
+                (ExpectedConditions.elementToBeClickable(By.cssSelector(".last.price")))
+                .getText();
+
+        // remove non-numeric characters if necessary
+        pomCartPrice1 = pomCartPrice1.replaceAll("\\D", ""); // Removes $, €, etc.
+
+        // Convert the string to an integer
+        int cartPrice1 = Integer.parseInt(pomCartPrice1);
 
         //assert item in the cart
         Assertions.assertEquals(expectedName,actualName);
-    }
 
+        //write output to the terminal
+        System.out.println("Name of item in the cart: ");
+        System.out.println(actualName); //write name of item in terminal
+
+        //add one item of the same television
+        browserWait.until
+                (ExpectedConditions.elementToBeClickable
+                        (By.cssSelector(".countPlus")))
+                .click();
+
+        //cart price - two items
+        var pomCartPrice2 = browserWait.until
+                        (ExpectedConditions.elementToBeClickable(By.cssSelector(".last.price")))
+                .getText();
+
+        // remove non-numeric characters if necessary
+        pomCartPrice2 = pomCartPrice2.replaceAll("\\D", ""); // Removes $, €, etc.
+
+        // convert the string to an integer
+        int cartPrice2 = Integer.parseInt(pomCartPrice2);
+
+        //assert double prices in the cart
+        Assertions.assertEquals(cartPrice1 * 2, cartPrice2);
+
+        //write output to the terminal
+        System.out.println("Price for one item: ");
+        System.out.println(cartPrice1); //write number for one item in terminal
+        System.out.println("Price for two item: ");
+        System.out.println(cartPrice2); //write number for two item in terminal
+    }
 
 }
