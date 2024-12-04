18da21e7da62f879d5e2004a42f643f5eeb6a7af
Zuzana Ondrackova <ondrac.zuzana@gmail.com>
2024-12-04 16:45:56 +0100

    Adding descriptions.

--- src/test/java/AlzaPageTest.java
+++ src/test/java/AlzaPageTest.java
@@ -4,14 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.List;
 
 public class AlzaPageTest {
 
@@ -104,9 +101,11 @@ public class AlzaPageTest {
         //add one item of the same television
         cartOperations.addOneItem();
 
+        //variable for present of disabled button
         var disabledConPlus = browserWait.until
                 (ExpectedConditions.presenceOfElementLocated(By.cssSelector(".countPlus.disabled")));
 
+        //condition for present of disabled button
         if(disabledConPlus.isDisplayed()) {
             // if yes, write a message
             System.out.println("Toto zboží je k dispozici pouze 1x.");
@@ -114,9 +113,7 @@ public class AlzaPageTest {
             // end the test
             System.out.println("Nelze přidat více než 1 kus do košíku.");
             return; // end the test
-
         }
-
 
         //cart price - two items
         var pomCartPrice2 = browserWait.until
